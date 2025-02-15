040b1ed178f139b22f6ab51bfa3a3dc48014af9c
sichi46 <willsonmulenga7@gmail.com>
2025-02-15 23:58:28 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+// Pages to create 
+
+//  / or home
+
+// feed
+
+// profile
+
+Route::get('/', [DashboardController::class, 'index      
+']);
